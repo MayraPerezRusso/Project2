1534acabdd54fcf8faca21f715f4f1dd3d7c2352
savorgnan5 <savorgnan5@aol.com>
2019-04-07 08:39:38 -0500

    more

--- immigration/app.py
+++ immigration/app.py
@@ -18,17 +18,16 @@ app = Flask(__name__)
 # # Database Setup
 # #################################################
 
-# app.config["SQLALCHEMY_DATABASE_URI"] = "DATA PATH HERE"
-# db = SQLAlchemy(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "DATA PATH HERE"
+db = SQLAlchemy(app)
 
-# # reflect an existing database into a new model
-# Base = automap_base()
+Base = automap_base()
 # # reflect the tables
-# Base.prepare(db.engine, reflect=True)
+Base.prepare(db.engine, reflect=True)
 
 # # Save references to each table
-# Samples_Metadata = Base.classes.sample_metadata
-# Samples = Base.classes.samples
+Samples_Metadata = Base.classes.sample_metadata
+Samples = Base.classes.samples
 
 
 @app.route("/")
